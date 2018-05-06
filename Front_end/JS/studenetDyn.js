e4132e003a31b86f5d7f0144ef8a030830a0b29c
donaldStud <37040239+donaldStud@users.noreply.github.com>
2018-05-06 12:01:00 +0200

    Add files via upload

---
+++ Front_end/JS/studenetDyn.js
@@ -0,0 +1,16 @@
+/*- Global Variables -*/
+var style_;
+
+/*-Function-*/
+
+function aff_date_maj() {
+	var date_maj = new Date(document.lastModified);
+	document.write(date_maj);
+}
+
+function copyrightcolored() { 
+  document.getElementById("small_").style.color = 'blue';
+}
+
+
+//alert('cool');
